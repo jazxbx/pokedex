7f927e4b342a2938711b7692f8ee92a1241a16fd
jazz <jazxbx@gmail.com>
2024-06-08 22:54:51 -0700

    components added

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <div className='bg-red-600 text-zinc-100 text-xl content-center p-4 font-semibold md:p-7 md:text-3xl'>
+    <div className='bg-blue-600 text-zinc-100 text-2xl content-center p-4 font-semibold md:p-9 md:text-3xl'>
       Pokémon
     </div>
   );
