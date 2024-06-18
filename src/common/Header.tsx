6f712f93ee777eee2fc58c3c6231d4954979f066
jazz <jazxbx@gmail.com>
2024-06-17 21:03:30 -0700

    add props to header

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,7 +1,16 @@
-export function Header() {
+function Header({
+  text,
+  children,
+}: {
+  text: string;
+  children: React.ReactNode;
+}) {
   return (
-    <div className='bg-blue-600 text-zinc-100 text-2xl content-center p-4 font-semibold md:p-9 md:text-3xl'>
-      Pokémon
+    <div className='bg-red-600 text-zinc-100 text-2xl content-center p-3 font-semibold flex justify-between  md:p-7 md:text-3xl'>
+      <div className='max-sm:hidden'>{text}</div>
+      <div>{children}</div>
     </div>
   );
 }
+
+export default Header;
