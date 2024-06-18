5f1cff7c7d5ae1974d693e231932ca03f2cc5944
jazz <jazxbx@gmail.com>
2024-06-17 21:03:48 -0700

    app component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import './tailwind.css';
 import Card from './components/Card';
 import Modal from './components/Modal';
 import PokemonInfo from './components/PokemonInfo';
+import Header from './common/Header';
+import Search from './components/Search';
+import Error from './common/Error';
 
 const App = () => {
   interface Pokemon {
@@ -26,12 +29,36 @@ const App = () => {
     }[];
   }
 
+  const backgrounds: { [key: string]: string } = {
+    fire: 'bg-red-500',
+    water: 'bg-blue-500',
+    grass: 'bg-green-500',
+    electric: 'bg-yellow-500',
+    psychic: 'bg-fuchsia-500',
+    dark: 'bg-black',
+    normal: 'bg-gray-400',
+    fighting: 'bg-orange-500',
+    flying: 'bg-blue-800',
+    poison: 'bg-purple-800',
+    ground: 'bg-yellow-800',
+    rock: 'bg-gray-800',
+    bug: 'bg-green-800',
+    ghost: 'bg-indigo-800',
+    steel: 'bg-gray-500',
+    fairy: 'bg-pink-300',
+    dragon: 'bg-indigo-500',
+  };
+  // state get pokemon query
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  const [search, setSearch] = useState<string>('');
+  const [filteredPokemon, setFilteredPokemon] = useState<Pokemon[]>([]);
+  //modal states
   const [selectedPokemon, setSelectedPokemon] = useState<Pokemon | null>(null);
   const [openModal, setOpenModal] = useState(false);
 
   const fetchAllPokemon = async () => {
-    const res = await fetch(`${BASE_URL}?limit=50`);
+    const res = await fetch(`${BASE_URL}?limit=150`);
     const data = await res.json();
     // console.log(data);
     const pokemonObject = data.results.map(async (pokemon: Pokemon) => {
@@ -53,8 +80,47 @@ const App = () => {
     setOpenModal(true);
   }
 
+  //when I type something on the search, I need to capture that value and check it against pokemon data via filter method?
+  function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
+    const searchQuery = event.target.value.toLowerCase();
+    setSearch(searchQuery);
+  }
+
+  function filterPokemon(pokemons: Pokemon[]) {
+    const filter = pokemons.filter((pokemon) => {
+      return pokemon.name.toLowerCase().includes(search);
+    });
+    setFilteredPokemon(filter);
+  }
+
+  useEffect(() => {
+    filterPokemon(pokemons);
+  }, [search, pokemons]);
+
   return (
     <main className='h-screen'>
+      <Header text='Pokemon'>
+        <Search placeholder='Search Pokemon...' onChange={handleSearch} />
+      </Header>
+      {filteredPokemon.length ? (
+        <section className='grid md:grid-cols-3 gap-4 p-8'>
+          {filteredPokemon.map((pokemon) => {
+            return (
+              <Card
+                key={pokemon.id}
+                id={pokemon.id}
+                name={pokemon.name}
+                image={pokemon.sprites.front_default}
+                types={pokemon.types}
+                backgrounds={backgrounds}
+                onClick={() => handleCardClick(pokemon)}
+              />
+            );
+          })}
+        </section>
+      ) : (
+        <Error />
+      )}
       <Modal open={openModal} setOpen={setOpenModal}>
         {selectedPokemon && (
           <PokemonInfo
@@ -63,24 +129,10 @@ const App = () => {
             img={selectedPokemon.sprites.front_default}
             types={selectedPokemon.types}
             stats={selectedPokemon.stats}
+            backgrounds={backgrounds}
           />
         )}
       </Modal>
-
-      <section className='grid md:grid-cols-3 gap-4 p-8'>
-        {pokemons.map((pokemon) => {
-          return (
-            <Card
-              key={pokemon.id}
-              id={pokemon.id}
-              name={pokemon.name}
-              image={pokemon.sprites.front_default}
-              types={pokemon.types}
-              onClick={() => handleCardClick(pokemon)}
-            />
-          );
-        })}
-      </section>
     </main>
   );
 };
